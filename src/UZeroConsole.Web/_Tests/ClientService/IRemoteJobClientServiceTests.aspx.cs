d404e813a523b57d7675365d2d291ba84d4eae7e
Mixiaop <112949824@qq.com>
2018-01-23 13:33:43 +0800

    1

--- src/UZeroConsole.Web/_Tests/ClientService/IRemoteJobClientServiceTests.aspx.cs
+++ src/UZeroConsole.Web/_Tests/ClientService/IRemoteJobClientServiceTests.aspx.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using U;
-using UZeroConsole.Client.Jobs;
+//using UZeroConsole.Client.Jobs;
 
 namespace UZeroConsole.Web._Tests.ClientService
 {
@@ -8,9 +8,9 @@ namespace UZeroConsole.Web._Tests.ClientService
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            IRemoteJobClientService clientService = UPrimeEngine.Instance.Resolve<IRemoteJobClientService>();
-            clientService.RunItem(2, "http://www.youzy.cn", "haha");
-            Response.Write("sus");
+            //IRemoteJobClientService clientService = UPrimeEngine.Instance.Resolve<IRemoteJobClientService>();
+            //clientService.RunItem(2, "http://www.youzy.cn", "haha");
+            //Response.Write("sus");
         }
     }
 }
