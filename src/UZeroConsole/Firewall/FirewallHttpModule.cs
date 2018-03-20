655f3975f0f76c9b2760c2d2ac650c9c5a234b81
Mixiaop <112949824@qq.com>
2018-03-20 13:44:23 +0800

    1

---
+++ src/UZeroConsole/Firewall/FirewallHttpModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace UZeroConsole.Firewall
+{
+    public class FirewallHttpModule : System.Web.IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            var a = 1;    
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
